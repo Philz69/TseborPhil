d9b53c692c3fbbe9772dedf021d387a81e67ec44
zacharythuotte <36003312+zacharythuotte@users.noreply.github.com>
2019-09-25 16:38:01 -0400

    a

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() 
+{
+  Serial.begin(9600);
+  delay(1500);
+}
+
+void loop() 
+{
+  Serial.println("Hello world!");
+
+  delay(100);
+  // put your main code here, to run repeatedly:
+}
